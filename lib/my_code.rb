3e92449285720d6f9769e77aabfe86b4fc5ba5f1
Gage Thornberry <gagedoug13@gmail.com>
2019-08-19 21:58:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,8 @@
 def map(array)
   i = 0
   new_array = []
-  while i < array.length do
-    new_array.push(map.array) {|n| n * -1}
+  while i < array.length 
+    new_array.push(yield(array[i]))
     i += 1
   end
   p new_array
